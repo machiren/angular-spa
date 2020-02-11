bd50567e8f45def9e8ad87cba93e30813b2b9e7e
[machiren] <[machiren111@gmail.com]>
2020-02-11 19:50:09 +0900

    layout作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LayoutModule } from "./layout/layout.module";
+import { IconsProviderModule } from './icons-provider.module';
+import { NgZorroAntdModule, NZ_I18N, ja_JP } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import ja from '@angular/common/locales/ja';
+
+registerLocaleData(ja);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    LayoutModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: ja_JP }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
